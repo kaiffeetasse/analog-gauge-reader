e2906724e16b8c70d179d2bebb18af54047adcff
kaiffeetasse <qyp@web.de>
2021-08-31 20:47:55 +0200

    refactor

--- main.py
+++ main.py
@@ -31,8 +31,7 @@ def measure_gauge_from_image(filename):
     dst = cv.Canny(src, 50, 200, None, 3)
 
     # Copy edges to the images that will display the results in BGR
-    cdst = cv.cvtColor(dst, cv.COLOR_GRAY2BGR)
-    cdstP = np.copy(cdst)
+    cdstP = cv.cvtColor(dst, cv.COLOR_GRAY2BGR)
 
     linesP = cv.HoughLinesP(dst, 1, np.pi / 180, 50, None, 50, 10)
 
@@ -41,15 +40,16 @@ def measure_gauge_from_image(filename):
 
     if linesP is not None:
         for i in range(0, len(linesP)):
-            l = linesP[i][0]
+            line = linesP[i][0]
 
-            if l[3] < LINES_ABOVE_Y:
-                cv.line(cdstP, (l[0], l[1]), (l[2], l[3]), (0, 0, 255), 3, cv.LINE_AA)
-                cv.putText(cdstP, str(counter), (l[2] + 5, l[3] + 5), cv.FONT_HERSHEY_PLAIN, 3, (0, 255, 0), 2)
-                logger.debug("line " + str(counter) + ": " + str(l[0]) + ", " + str(l[1]))
-                logger.debug("line " + str(counter) + ": " + str(l[2]) + ", " + str(l[3]))
+            if line[3] < LINES_ABOVE_Y:
+                cv.line(cdstP, (line[0], line[1]), (line[2], line[3]), (0, 0, 255), 3, cv.LINE_AA)
+                cv.putText(cdstP, str(counter), (line[2] + 5, line[3] + 5), cv.FONT_HERSHEY_PLAIN, 3, (0, 255, 0), 2)
 
-                angle = math.degrees(math.atan2(l[0] - l[1], l[2] - [3]))
+                logger.debug("line " + str(counter) + ": " + str(line[0]) + ", " + str(line[1]))
+                logger.debug("line " + str(counter) + ": " + str(line[2]) + ", " + str(line[3]))
+
+                angle = math.degrees(math.atan2(line[0] - line[1], line[2] - [3]))
                 logger.debug("line " + str(counter) + " angle = " + str(angle))
                 angles.append(angle)
 
@@ -66,7 +66,6 @@ def measure_gauge_from_image(filename):
     logger.info("bar = " + str(bar))
 
     cv.waitKey()
-    return 0
 
 
 if __name__ == "__main__":
